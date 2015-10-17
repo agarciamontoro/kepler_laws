efa5b5ce3dd1f87133a69ddcbccdcdd16d6d23a5
Alejandro Garcia Montoro <alejandro.garciamontoro@gmail.com>
2015-10-17 22:10:05 +0200

    Aúna todos los métodos en la clase

--- LeapDriver.py
+++ LeapDriver.py
@@ -6,29 +6,6 @@ from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 
 new_frame = [False, False] #Guarda si hay un nuevo frame para la mano izqda. y la dcha.
 
-def fingerPos(hand, handType):
-	fingers = hand.fingers
-	num_finger = 0
-	# Comprueba si la mano tiene algún dedo
-	if not fingers.is_empty:
-		# Calcula la posición media de los dedos de la mano
-
-		for finger in fingers:
-			matrix1[handType][num_finger] = finger.tip_position
-			num_finger += 1
-			print "num_finger", num_finger
-
-		avg_pos = num_finger
-		print "Hand has %d fingers" %(len(fingers))
-		print "finger tip position1 is: ", matrix1[handType][0]
-		print "finger tip position2 is: ", matrix1[handType][1]
-		print "finger tip position3 is: ", matrix1[handType][2]
-		print "finger tip position4 is: ", matrix1[handType][3]
-		print "finger tip position5 is: ", matrix1[handType][4]
-
-		num_finger = 0
-	global matrix1
-
 class SampleListener(Leap.Listener):
 	def on_init(self, controller):
 		matrix1 = [[Leap.Vector() for i in xrange(5)] for i in xrange(2)]
@@ -51,6 +28,32 @@ class SampleListener(Leap.Listener):
 	def on_exit(self, controller):
 		print "Exited"
 
+	def fingerPos(self, hand, handType):
+		fingers = hand.fingers
+		num_finger = 0
+		# Comprueba si la mano tiene algún dedo
+		if not fingers.is_empty:
+			# Calcula la posición media de los dedos de la mano
+
+			for finger in fingers:
+				matrix1[handType][num_finger] = finger.tip_position
+				num_finger += 1
+				print "num_finger", num_finger
+
+			avg_pos = num_finger
+			print "Hand has %d fingers" %(len(fingers))
+			print "finger tip position1 is: ", matrix1[handType][0]
+			print "finger tip position2 is: ", matrix1[handType][1]
+			print "finger tip position3 is: ", matrix1[handType][2]
+			print "finger tip position4 is: ", matrix1[handType][3]
+			print "finger tip position5 is: ", matrix1[handType][4]
+
+			num_finger = 0
+		global matrix1
+
+	def getHands(self):
+
+
 	def on_frame(self, controller):
 		# Obtiene el frame mas reciente y proporciona información básica
 		frame = controller.frame()
@@ -65,7 +68,7 @@ class SampleListener(Leap.Listener):
 			for i in xrange(2):
 				if frame.hands[i].is_valid:
 					new_frame[i] = True
-					fingerPos(frame.hands[i], i)
+					self.fingerPos(frame.hands[i], i)
 
 		global new_frame
 		global matrix1
@@ -82,6 +85,7 @@ class SampleListener(Leap.Listener):
 
 		if state == Leap.Gesture.STATE_INVALID:
 			return "STATE_INVALID"
-
+'''
 def getHands():
 	return new_frame, matrix1
+'''
