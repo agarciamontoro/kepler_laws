1bcc01332bf544255e12a5bb72493259b39f2902
Alejandro Garcia Montoro <alejandro.garciamontoro@gmail.com>
2015-11-05 18:09:58 +0100

    Counting procedure implemented

--- gestures.py
+++ gestures.py
@@ -1,11 +1,13 @@
 import Leap
 from Leap import Finger
 
+import math
+
 # Distance
 def distance(pos1,pos2,weight=[1.0,1.0,1.0]):
     return math.sqrt(sum([weight[i]*(pos2[i]-pos1[i])**2 for i in range(3)]))
 
-# Returns wether a hand is doing an OK gesture
+# Returns whether a hand is doing an OK gesture
 def isGestureOK(hand, tolerance = 10):
     gesture = False
     if hand.is_valid:
@@ -17,14 +19,19 @@ def isGestureOK(hand, tolerance = 10):
 
     return gesture
 
+# Returns whether a hand is counting
 def isGestureCounting(hand):
-	return distance(thumb.tip_position, hand.palm_position, [1.0,0.0,1.0]) < hand.palm_width/2.0:
+    thumb = hand.fingers.finger_type(Finger.TYPE_THUMB)[0]
+    return distance(thumb.tip_position, hand.palm_position, [1.0,0.0,1.0]) < hand.palm_width/2.0
 
-# Return the number shown by the fingers
+def isFingerCounting(hand,finger):
+    return distance(finger.tip_position, hand.palm_position, [0,0,1.0]) > hand.palm_width/2.0
+
+# Returns the number shown by the fingers when the hand is counting
 def fingerCount(hand):
-	count = 0
-	if hand.is_valid:
-		thumb = hand.fingers.finger_type(Finger.TYPE_THUMB)[0]
-		if isGestureCounting(hand):   
+    if hand.is_valid and isGestureCounting(hand):
+        count = sum( (1 if isFingerCounting(hand,finger) else 0 for finger in hand.fingers) )
+    else:
+        count = -1
 
-	return count
+    return count
