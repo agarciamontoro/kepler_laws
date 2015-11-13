f238e64408cbcd487ea80ac20282807f0a4593e4
Alejandro Garcia Montoro <alejandro.garciamontoro@gmail.com>
2015-11-13 14:45:57 +0100

    Adds an Image object to fill the GLUT window whenever required.

--- primitives.py
+++ primitives.py
@@ -4,9 +4,72 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
+# NEEDS PYTHON2-PILLOW
+import PIL.Image
+
+from collections import namedtuple
+
 import math
 
 from constants import *
+
+class Image:
+    def __init__(self, img_file_name):
+        image = PIL.Image.open(img_file_name)
+        self.raw_data = image.tobytes("raw", "RGBA", 0, -1)
+        self.width, self.height = image.size
+
+        glEnable( GL_TEXTURE_2D )
+
+        # Texture identifier assignment and binding
+        self.id_text = glGenTextures( 1 )
+        glBindTexture( GL_TEXTURE_2D, self.id_text )
+
+        # Texture mipmaps
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE)
+
+        # Send texels to GPU
+        # Params: flag, mipmap level, intern format, width, height, border size, texels format, texel type, texture
+        glTexImage2D(GL_TEXTURE_2D,0,3,self.width,self.height,0,GL_RGBA,GL_UNSIGNED_BYTE,self.raw_data)
+
+        # Unbinding
+        glBindTexture(GL_TEXTURE_2D, 0)
+        glDisable( GL_TEXTURE_2D )
+
+    def draw(self):
+        window_size = (glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT))
+
+        glMatrixMode(GL_PROJECTION)
+        glPushMatrix()
+        glLoadIdentity()
+        glOrtho(0.0, window_size[0], 0.0, window_size[1], -1.0, 1.0)
+        glMatrixMode(GL_MODELVIEW)
+        glPushMatrix()
+
+        glLoadIdentity()
+
+        glColor3f(1,1,1)
+        glEnable(GL_TEXTURE_2D)
+        glBindTexture(GL_TEXTURE_2D, self.id_text)
+
+        # Draw a textured quad
+        glBegin(GL_QUADS)
+        glTexCoord2f(0, 0); glVertex3f(0, 0, 0)
+        glTexCoord2f(0, 1); glVertex3f(0, window_size[1], 0)
+        glTexCoord2f(1, 1); glVertex3f(window_size[0], window_size[1], 0)
+        glTexCoord2f(1, 0); glVertex3f(window_size[0], 0, 0)
+        glEnd()
+
+        glDisable(GL_TEXTURE_2D)
+        glPopMatrix()
+
+        glMatrixMode(GL_PROJECTION)
+        glPopMatrix()
+
+        glMatrixMode(GL_MODELVIEW)
+
 
 class Line:
     def __init__(self, points, color):
