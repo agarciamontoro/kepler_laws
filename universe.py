a672cb245b10d1f4ce21508dc0f4f8e0a3927902
Alejandro Garcia Montoro <alejandro.garciamontoro@gmail.com>
2015-12-03 12:05:27 +0100

    Adds planet and universe files

---
+++ universe.py
@@ -0,0 +1,37 @@
+import primitives
+from planet import Planet
+from constants import *
+
+import itertools, math
+
+def function_u(t):
+    # TODO
+    return t*0.001*math.pi
+
+def bigBang():
+    global planets, time
+
+    planetMercury = Planet(0.387, 0.206, function_u, BALL_RADIUS, 87.97, "Mercury")
+    planetVenus = Planet(0.723, 0.007, function_u, BALL_RADIUS, 224.7, "Venus")
+    planetEarth = Planet(1., 0.017, function_u, BALL_RADIUS, 365.26, "Earth")
+    planetMars = Planet(1.524, 0.093, function_u, BALL_RADIUS, 686.98, "Mars")
+    planetJupiter = Planet(5.203, 0.048, function_u, BALL_RADIUS, 4332.6, "Jupiter")
+    planetSaturn = Planet(9.546, 0.056, function_u, BALL_RADIUS, 10759, "Saturn")
+    planetUranus = Planet(19.2, 0.047, function_u, BALL_RADIUS, 30687, "Uranus")
+    planetNeptune = Planet(30.09, 0.009, function_u, BALL_RADIUS, 60784, "Neptune")
+
+    planets = [planetMercury, planetVenus, planetEarth, planetMars, planetJupiter, planetSaturn, planetUranus, planetNeptune]
+
+    time = 0
+
+def processFrame():
+    global time
+    time = time + 1
+
+    objects = []
+
+    for planet in planets:
+        planet.setPos(time)
+        objects.append(planet)
+
+    return objects
