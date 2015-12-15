d380a36d325eaa9a93ce37d2e681bb6a5c90db3d
asolisi <asolisi.1993@gmail.com>
2015-12-15 19:12:34 +0100

    casi acabau

--- wxGUI.py
+++ wxGUI.py
@@ -15,62 +15,15 @@ current_date = datetime.today()
 
 class MyDialog(wx.Dialog):
     def __init__(self, parent, id, title):
-        wx.Dialog.__init__(self, parent, id, title, size=(900,500), style=wx.DEFAULT_DIALOG_STYLE)
+        wx.Dialog.__init__(self, parent, id, title, size=(1000,250), style=wx.DEFAULT_DIALOG_STYLE)
 
         #Complete Panel
         hbox  = wx.BoxSizer(wx.HORIZONTAL)
 
-        #Left Panel
-        vbox1 = wx.BoxSizer(wx.VERTICAL)
+        vbox0 = wx.BoxSizer(wx.VERTICAL)
 
-
-        pnl1 = wx.Panel(self, -1, style=wx.SIMPLE_BORDER)
-        pnl2 = wx.Panel(self, -1, style=wx.SIMPLE_BORDER)
         pnl3 = wx.Panel(self, -1, style=wx.SIMPLE_BORDER)
-        vbox1.Add(pnl1, 1, wx.EXPAND | wx.ALL, 3)
-        vbox1.Add(pnl2, 1, wx.EXPAND | wx.ALL, 3)
-        vbox1.Add(pnl3, 1, wx.EXPAND | wx.ALL, 3)
-
-
-        #Date Panel
-        self.tc_date_dd = wx.SpinCtrl(pnl1, -1, '15', (55,90), (60,-1), min=1, max=31)
-        self.tc_date_mm = wx.SpinCtrl(pnl1, -1, '12', (55,90), (60,-1), min=1, max=12)
-        self.tc_date_yy = wx.SpinCtrl(pnl1, -1, '2015', (55,90), (60,-1), min=1, max=9999)
-        self.main_calc_but = wx.Button(pnl1, -1, 'Calculate')
-
-        grid1 = wx.GridSizer(2,4,0,0)
-        grid1.AddMany([ (wx.StaticText(pnl1, -1, 'Date'),0, wx.ALIGN_CENTER),
-                        (self.tc_date_dd, 0, wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL),
-                        (self.tc_date_mm, 0, wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL),
-                        (self.tc_date_yy, 0, wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL),(0,0),
-                        (self.main_calc_but,   0, wx.ALIGN_CENTER| wx.BOTTOM)])
-
-        pnl1.SetSizer(grid1)
-        self.Bind(wx.EVT_BUTTON, self.OnCalculate, id=self.main_calc_but.GetId())
-
-
-
-        #Panel del medio
-        planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus','Neptune']
-
-        self.tc_ecc = wx.TextCtrl(pnl2, -1)
-        self.combo = wx.ComboBox(pnl2, -1, choices=planets, style=wx.CB_READONLY)
-        self.result =  wx.StaticText(pnl2, -1, '')
-        self.main_calc_date_but = wx.Button(pnl2, -1, 'CalculateDate')
-
-        grid2 = wx.GridSizer(2,4,0,0)
-        grid2.AddMany([ (wx.StaticText(pnl2, -1, 'Ecc. anomaly'),0, wx.ALIGN_CENTER),
-                        (self.tc_ecc, 0, wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL),
-                        (wx.StaticText(pnl2, -1, 'for'),0, wx.ALIGN_CENTER),
-                        (self.combo, 0, wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL),
-                        (self.main_calc_date_but, 0, wx.ALIGN_CENTER| wx.BOTTOM),(0,0),
-                        (self.result, 0, wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL)])
-
-        pnl2.SetSizer(grid2)
-        self.Bind(wx.EVT_BUTTON, self.OnCalculateDate, id=self.main_calc_date_but.GetId())
-
-
-        #Rigth Panel
+        vbox0.Add(pnl3, 1, wx.EXPAND | wx.ALL, 3)
 
         #CheckBoxes
         checkbox = wx.BoxSizer(wx.VERTICAL)
@@ -111,6 +64,55 @@ class MyDialog(wx.Dialog):
         pnl3.SetSizer(checkbox)
 
 
+        #Center Panel
+        vbox1 = wx.BoxSizer(wx.VERTICAL)
+
+
+        pnl1 = wx.Panel(self, -1, style=wx.SIMPLE_BORDER)
+        pnl2 = wx.Panel(self, -1, style=wx.SIMPLE_BORDER)
+
+        vbox1.Add(pnl1, 1, wx.EXPAND | wx.ALL, 3)
+        vbox1.Add(pnl2, 1, wx.EXPAND | wx.ALL, 3)
+
+
+        #Date Panel
+        self.tc_date_dd = wx.SpinCtrl(pnl1, -1, '15', (55,90), (60,-1), min=1, max=31)
+        self.tc_date_mm = wx.SpinCtrl(pnl1, -1, '12', (55,90), (60,-1), min=1, max=12)
+        self.tc_date_yy = wx.SpinCtrl(pnl1, -1, '2015', (55,90), (60,-1), min=1, max=9999)
+        self.main_calc_but = wx.Button(pnl1, -1, 'Calculate all')
+
+        grid1 = wx.GridSizer(2,4,0,0)
+        grid1.AddMany([ (wx.StaticText(pnl1, -1, 'Date'),0, wx.ALIGN_CENTER),
+                        (self.tc_date_dd, 0, wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL),
+                        (self.tc_date_mm, 0, wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL),
+                        (self.tc_date_yy, 0, wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL),(0,0),
+                        (self.main_calc_but,   0, wx.ALIGN_CENTER| wx.BOTTOM)])
+
+        pnl1.SetSizer(grid1)
+        self.Bind(wx.EVT_BUTTON, self.OnCalculate, id=self.main_calc_but.GetId())
+
+
+
+        #Ecc panel
+        planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus','Neptune']
+
+        self.tc_ecc = wx.TextCtrl(pnl2, -1)
+        self.combo = wx.ComboBox(pnl2, -1, choices=planets, style=wx.CB_READONLY)
+        self.result =  wx.StaticText(pnl2, -1, '')
+        self.main_calc_date_but = wx.Button(pnl2, -1, 'Calculate date')
+
+        grid2 = wx.GridSizer(2,3,0,0)
+        grid2.AddMany([ (wx.StaticText(pnl2, -1, 'Ecc. anomaly'),0, wx.ALIGN_CENTER),
+                        (self.tc_ecc, 0, wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL),
+                        (self.combo, 0, wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL),
+                        (self.main_calc_date_but, 0, wx.ALIGN_CENTER| wx.BOTTOM),
+                        (self.result, 0, wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL)])
+
+        pnl2.SetSizer(grid2)
+        self.Bind(wx.EVT_BUTTON, self.OnCalculateDate, id=self.main_calc_date_but.GetId())
+
+
+        #Rigth Panel
 
         vbox2 = wx.BoxSizer(wx.VERTICAL)
 
@@ -120,12 +122,14 @@ class MyDialog(wx.Dialog):
         self.lc.InsertColumn(1, 'Anomaly for t')
         self.lc.InsertColumn(2, 'Energy')
         self.lc.InsertColumn(3, 'Momentum')
-        #self.lc.SetColumnWidth(0, 140)
+        self.lc.SetColumnWidth(1, 120)
+        self.lc.SetColumnWidth(2, 150)
+        self.lc.SetColumnWidth(3, 150)
 
         vbox2.Add(self.lc, 1, wx.EXPAND | wx.ALL, 3)
 
 
-
+        hbox.Add(vbox0, 1, wx.EXPAND)
         hbox.Add(vbox1, 1, wx.EXPAND)
         hbox.Add(vbox2, 1, wx.EXPAND)
         self.SetSizer(hbox)
